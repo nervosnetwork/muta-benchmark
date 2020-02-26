c959b254ed58555daf2e1d5ddfbc9b7fd9bccafe
homura <homura.dev@gmail.com>
2020-02-26 16:35:03 +0800

    fix: fix failure caused by custom `gap` or `chain-id`

--- index.js
+++ index.js
@@ -3,24 +3,38 @@
 const program = require("commander");
 const ora = require("ora");
 const cluster = require("cluster");
+const { utils } = require("muta-sdk");
 const { runMain, runWorker } = require("./bench");
 const logger = require("./logger");
 const { AssetBenchProducer } = require("./BenchProducer");
 
+const toNumber = v => parseInt(v);
+
 const { args } = program
   .option("-m --method [method]", "HTTP method", "POST")
-  .option("-d --duration [duration]", "number of second", 60)
-  .option("-g --gap [gap]", "muta's timeout_gap", 20)
-  .option("-c --connections [connections]", "number of connection", 20)
+  .option("-d --duration [duration]", "number of second", toNumber, 60)
+  .option("-g --gap [gap]", "muta's timeout_gap", toNumber, 20)
+  .option("-c --connections [connections]", "number of connection", toNumber, 20)
   .option(
     "--pk [privateKey]",
     "sender of the transfer",
+    utils.toHex,
     process.env.PRIVATE_KEY || "0x45c56be699dca666191ad3446897e0f480da234da896270202514a0e1a587c3f"
   )
-  .option("--chain-id [chainId]", "chain id", "0xb6a4d7da21443f5e816e8700eea87610e6d769657d6b8ec73028457bf2ca4036")
-  .option("--receiver [receiver]", "receiver of the transfer", "0x103e9b982b443592ffc3d4c2a484c220fb3e29e2e4")
+  .option(
+    "--chain-id [chainId]",
+    "chain id",
+    utils.toHex,
+    "0xb6a4d7da21443f5e816e8700eea87610e6d769657d6b8ec73028457bf2ca4036"
+  )
+  .option(
+    "--receiver [receiver]",
+    "receiver of the transfer",
+    utils.toHex,
+    "0x103e9b982b443592ffc3d4c2a484c220fb3e29e2e4"
+  )
   .option("--verbose", "show verbose info, use it for debug", false)
-  .option("--cpu [cpu]", "cpu nums", 3)
+  .option("--cpu [cpu]", "cpu nums", toNumber, 3)
   .option("--json", "print output as json", false)
   .name("muta-bench")
   .usage(
@@ -38,17 +52,17 @@ if (opts.verbose) {
 }
 
 if (cluster.isMaster) {
-  run()
-    .catch(({ errorCount }) => {
-      if (errorCount > 0) {
-        console.error(`HTTP success but GraphQL error count: ${errorCount}`);
-      }
-    });
+  run().catch(({ errorCount }) => {
+    if (errorCount > 0) {
+      console.error(`HTTP success but GraphQL error count: ${errorCount}`);
+    }
+  });
 } else {
   runWorker();
 }
 
 async function run() {
+  console.log(opts);
   const { gap, pk, url, receiver, chainId } = opts;
   const assetBenchProducer = new AssetBenchProducer({
     pk,
@@ -69,7 +83,7 @@ async function run() {
   await assetBenchProducer.prepare();
 
   opts.headers = {};
-  opts.headers['Content-Type'] = "application/json";
+  opts.headers["Content-Type"] = "application/json";
 
   const workers = Array.from({ length: opts.cpu }).map(() => {
     const worker = cluster.fork({
